393f47330420455e8786e915b8af6e017887d4e7
spriha <codesmiths.esspl@gmail.com>
2021-05-03 00:45:44 +0530

    added UI for I/O

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('button').click(function(e){
+        e.preventDefault()
+        const location = $('input').value
+        const url = '/weather?address=' + location
+
+        $('#message-1').text('Loading...')
+        $('#message-2').text('')
+
+        fetch(url).then((response) => {
+            response.json().then((data) => {
+                if( data.error ) {
+                    $('#message-1').text(data.error)
+                } else {
+                    $('#message-1').text(data.location)
+                    $('#message-2').text(data.forecast)
+                }
+
+            })
+        })
+    })
+})
